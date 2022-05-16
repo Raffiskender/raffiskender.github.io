d0ddf2da4c3dc8d1c6d576df912be2ad1598eed6
raffiskender <raffiskender@gmail.com>
2022-05-16 22:04:36 +0300

    Ajout de couleurs et programation de celles-ci

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const animations = {
 
         const bouttonLanguette = document.querySelector('.theme-bar--languette');
         bouttonLanguette.addEventListener('click', animations.themeChoicesAppear);
-        const bouttonThemeLight = document.querySelector('.theme-light');
-        const bouttonThemeDark = document.querySelector('.theme-dark');
-        bouttonThemeLight.addEventListener('click', animations.themeSwitch);
-        bouttonThemeDark.addEventListener('click', animations.themeSwitch);
-        // bouttonLanguette.classList.toggle("theme-bar--languette-ouverte")
-        
+        const bouttonThemeNightOrDay = document.querySelector('.theme-color-nightOrDay');
+        bouttonThemeNightOrDay.addEventListener('click', animations.themeSwitch);
+        const bouttonThemeColor = document.querySelectorAll('.theme-color');
+
+        for (const currentButton of bouttonThemeColor){
+            currentButton.addEventListener('click', animations.changeThemeColor);
+        }
     },
 
     themeChoicesAppear : function(event) {
@@ -20,22 +21,44 @@ const animations = {
 
     themeSwitch : function(event){
         clickedButton = event.currentTarget;
-        console.log('Dans la fonction');
 
-        if (clickedButton.classList == 'theme-color theme-light'){
+        if (clickedButton.classList == 'theme-color-nightOrDay theme-light'){
             clickedButton.classList.remove("theme-light");
             clickedButton.classList.add("theme-dark");
-            document.querySelector('body').classList.add('dark');
+        
+            document.querySelector('body').classList.add("dark");
         }
         else {
             clickedButton.classList.remove("theme-dark");
             clickedButton.classList.add("theme-light");
+            
             document.querySelector('body').classList.remove('dark');
-
         }
 
-    }
+    },
 
+    changeThemeColor : function(event){
+
+        clickedButton = event.currentTarget;
+
+        document.querySelector('body').classList.remove('green', 'blue', 'red', 'violet', 'f0f');
+
+        if (clickedButton.classList.value == 'theme-color blue'){
+            document.querySelector('body').classList.add('blue');
+        }
+        else if (clickedButton.classList.value == 'theme-color red'){
+            document.querySelector('body').classList.add('red');
+        }
+        else if (clickedButton.classList.value == 'theme-color green'){
+            document.querySelector('body').classList.add('green');
+        }
+        else if (clickedButton.classList.value == 'theme-color violet'){
+            document.querySelector('body').classList.add('violet');
+        }
+        else {
+            document.querySelector('body').classList.add('f0f');
+        }
+    },
 };
 
 document.addEventListener('DOMContentLoaded', animations.init)
